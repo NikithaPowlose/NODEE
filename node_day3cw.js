74f8add7b003afc787f1ea5ed121c5f359305081
Nikitha <nikithaa015@gmail.com>
2025-11-11 19:36:05 +0530

    day3_cw

---
+++ node_day3cw.js
@@ -0,0 +1,11 @@
+let studentName="Rahul";
+let correctAnswers=4;
+let wrongAnswers=1;
+
+function getFinalScore(name,correct,wrong){
+    return name==="Unknown"?"invalid Student":(correct*4)-(wrong*1);
+}
+let result=getFinalScore(studentName,correctAnswers,wrongAnswers);
+
+console.log(result);
+console.log(typeof result);
